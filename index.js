aba223a09bbde902064c63ccec2f63ad74052dfa
Chelsoconn <c.oconnor1517@gmail.com>
2024-11-17 14:10:02 -0600

    Add frontend build to backend

--- index.js
+++ index.js
@@ -23,6 +23,14 @@ app.use(cors())
     content: String,
     important: Boolean,
   })
+
+  noteSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
+  })
   
   const Note = mongoose.model('Note', noteSchema)
 //
