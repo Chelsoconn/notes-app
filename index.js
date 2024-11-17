d9627327d6cd0b62f50ef183ccb04945be05d799
Chelsoconn <c.oconnor1517@gmail.com>
2024-11-17 15:04:01 -0600

    Add frontend build to backend

--- index.js
+++ index.js
@@ -7,32 +7,8 @@ app.use(cors())
 
 
 //MONGO DATABASE LOGIC 
-  require('dotenv').config(); 
-  const mongoose = require('mongoose')
-  
-  const url = process.env.MONGODB_URI;
-    
-  mongoose.set('strictQuery',false)
-  
-  mongoose
-  .connect(url)
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.error('Error connecting to MongoDB:', err.message));
-  
-  const noteSchema = new mongoose.Schema({
-    content: String,
-    important: Boolean,
-  })
-
-  noteSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
-  })
-  
-  const Note = mongoose.model('Note', noteSchema)
+  require('dotenv').config();
+  const Note = require('./models/note')
 //
 
 
